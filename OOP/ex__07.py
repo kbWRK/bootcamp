46a8a851fb4592b068db5c34480b6cda60dff22b
Your Name <ksawery.boroszko@gmail.com>
2019-12-22 16:04:03 +0100

    22.12

---
+++ OOP/ex__07.py
@@ -0,0 +1,29 @@
+class Vector:
+    def __init__(self,x , y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+         return f"vector ({self.x}, {self.y})"
+
+    def __add__(self,other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, other):
+        if isinstance(other,Vector):
+            return Vector(self.x * other.x, self.y * other.y)
+        else:
+            return Vector(self.x * other, self.y * other)
+
+   def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+def test_vector_init():
+    v1 = Vector(1, 2)
+    assert v1.x == 1
+    assert v1.y == 2
+def test_vector_str():
+    v1 = Vector(1, 2)
+    assert str(v1) == "Vector(1, 2)"
+def test_vector_add():
+    v1 = Vector(1, 2)
